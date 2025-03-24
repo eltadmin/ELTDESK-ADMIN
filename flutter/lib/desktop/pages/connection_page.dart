d3158f168ee1f681f1c7b3a149deee2596cc6b30
dongepi <sysadmin@eltrade.com>
2025-03-24 14:11:57 +0200

    UP UI

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -4,12 +4,20 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hbb/common/widgets/connection_page_title.dart';
 import 'package:flutter_hbb/consts.dart';
 import 'package:flutter_hbb/models/state_model.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:flutter_hbb/models/peer_model.dart';
 
-// Запазваме OnlineStatusWidget, ако искаш да остане статусът
+import '../../common.dart';
+import '../../common/formatter/id_formatter.dart';
+import '../../common/widgets/peer_tab_page.dart';
+import '../../common/widgets/autocomplete.dart';
+import '../../models/platform_model.dart';
+
 class OnlineStatusWidget extends StatefulWidget {
   const OnlineStatusWidget({Key? key, this.onSvcStatusChanged})
       : super(key: key);
@@ -20,6 +28,7 @@ class OnlineStatusWidget extends StatefulWidget {
   State<OnlineStatusWidget> createState() => _OnlineStatusWidgetState();
 }
 
+/// State for the connection page.
 class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
   final _svcStopped = Get.find<RxBool>(tag: 'stop-service');
   final _svcIsUsingPublicServer = true.obs;
@@ -181,18 +190,109 @@ class ConnectionPage extends StatefulWidget {
 }
 
 /// State for the connection page.
-class _ConnectionPageState extends State<ConnectionPage> {
+class _ConnectionPageState extends State<ConnectionPage>
+    with SingleTickerProviderStateMixin, WindowListener {
+  final _idController = IDTextEditingController();
+  final RxBool _idInputFocused = false.obs;
+  final FocusNode _idFocusNode = FocusNode();
+  final TextEditingController _idEditingController = TextEditingController();
+  bool isWindowMinimized = false;
+  final AllPeersLoader _allPeersLoader = AllPeersLoader();
+
+  @override
+  void initState() {
+    super.initState();
+    _allPeersLoader.init(setState);
+    _idFocusNode.addListener(onFocusChanged);
+    if (_idController.text.isEmpty) {
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        final lastRemoteId = await bind.mainGetLastRemoteId();
+        if (lastRemoteId != _idController.id) {
+          setState(() {
+            _idController.id = lastRemoteId;
+          });
+        }
+      });
+    }
+    Get.put<TextEditingController>(_idEditingController);
+    Get.put<IDTextEditingController>(_idController);
+    windowManager.addListener(this);
+  }
+
+  @override
+  void dispose() {
+    _idController.dispose();
+    windowManager.removeListener(this);
+    _allPeersLoader.clear();
+    _idFocusNode.removeListener(onFocusChanged);
+    _idFocusNode.dispose();
+    _idEditingController.dispose();
+    if (Get.isRegistered<IDTextEditingController>()) {
+      Get.delete<IDTextEditingController>();
+    }
+    if (Get.isRegistered<TextEditingController>()) {
+      Get.delete<TextEditingController>();
+    }
+    super.dispose();
+  }
+
+  @override
+  void onWindowEvent(String eventName) {
+    super.onWindowEvent(eventName);
+    if (eventName == 'minimize') {
+      isWindowMinimized = true;
+    } else if (eventName == 'maximize' || eventName == 'restore') {
+      if (isWindowMinimized && isWindows) {
+        Get.forceAppUpdate();
+      }
+      isWindowMinimized = false;
+    }
+  }
+
+  @override
+  void onWindowEnterFullScreen() {
+    stateGlobal.resizeEdgeSize.value = 0;
+  }
+
+  @override
+  void onWindowLeaveFullScreen() {
+    stateGlobal.resizeEdgeSize.value = stateGlobal.isMaximized.isTrue
+        ? kMaximizeEdgeSize
+        : windowResizeEdgeSize;
+  }
+
+  @override
+  void onWindowClose() {
+    super.onWindowClose();
+    bind.mainOnMainWindowClose();
+  }
+
+  void onFocusChanged() {
+    _idInputFocused.value = _idFocusNode.hasFocus;
+    if (_idFocusNode.hasFocus) {
+      if (_allPeersLoader.needLoad) {
+        _allPeersLoader.getAllPeers();
+      }
+      final textLength = _idEditingController.value.text.length;
+      _idEditingController.selection =
+          TextSelection(baseOffset: 0, extentOffset: textLength);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final isOutgoingOnly = bind.isOutgoingOnly();
-    return Column(
-      children: [
-        Expanded(
-          child: Container(), // Празен контейнер, за да премахнем дясната част
-        ),
-        if (!isOutgoingOnly) const Divider(height: 1),
-        if (!isOutgoingOnly) OnlineStatusWidget(), // Запазваме статусния индикатор
-      ],
-    );
+    return SizedBox(); // Премахваме целия UI на десния панел
+  }
+
+  /// Callback за бутона за свързване (запазен, но не се използва в UI)
+  void onConnect({bool isFileTransfer = false, bool isViewCamera = false}) {
+    var id = _idController.id;
+    connect(context, id,
+        isFileTransfer: isFileTransfer, isViewCamera: isViewCamera);
+  }
+
+  /// UI за полето за отдалечен ID (запазен, но не се използва)
+  Widget _buildRemoteIDTextField(BuildContext context) {
+    return SizedBox(); // Празен контейнер, за да не се рендира нищо
   }
 }
