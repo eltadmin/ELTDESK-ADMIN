2b822170e11f8d832717512ec68e28020f48dce7
dongepi <sysadmin@eltrade.com>
2025-03-24 13:45:51 +0200

    Update connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -4,21 +4,12 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hbb/common/widgets/connection_page_title.dart';
 import 'package:flutter_hbb/consts.dart';
 import 'package:flutter_hbb/models/state_model.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher_string.dart';
-import 'package:window_manager/window_manager.dart';
-import 'package:flutter_hbb/models/peer_model.dart';
 
-import '../../common.dart';
-import '../../common/formatter/id_formatter.dart';
-import '../../common/widgets/peer_tab_page.dart';
-import '../../common/widgets/autocomplete.dart';
-import '../../models/platform_model.dart';
-import '../widgets/button.dart';
-
+// Запазваме OnlineStatusWidget, ако искаш да остане статусът
 class OnlineStatusWidget extends StatefulWidget {
   const OnlineStatusWidget({Key? key, this.onSvcStatusChanged})
       : super(key: key);
@@ -29,13 +20,12 @@ class OnlineStatusWidget extends StatefulWidget {
   State<OnlineStatusWidget> createState() => _OnlineStatusWidgetState();
 }
 
-/// State for the connection page.
 class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
   final _svcStopped = Get.find<RxBool>(tag: 'stop-service');
   final _svcIsUsingPublicServer = true.obs;
   Timer? _updateTimer;
 
-  double get em => 14.0; // Коригиран ред
+  double get em => 14.0;
   double? get height => bind.isIncomingOnly() ? null : em * 3;
 
   void onUsePublicServerGuide() {
@@ -127,10 +117,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
               width: isIncomingOnly ? 226 : null,
               child: _buildConnStatusMsg(),
             ),
-            // stop
             if (!isIncomingOnly) startServiceWidget(),
-            // ready && public
-            // No need to show the guide if is custom client.
             if (!isIncomingOnly) setupServerWidget(),
           ],
         );
@@ -194,140 +181,18 @@ class ConnectionPage extends StatefulWidget {
 }
 
 /// State for the connection page.
-class _ConnectionPageState extends State<ConnectionPage>
-    with SingleTickerProviderStateMixin, WindowListener {
-  /// Controller for the id input bar.
-  final _idController = IDTextEditingController();
-
-  final RxBool _idInputFocused = false.obs;
-  final FocusNode _idFocusNode = FocusNode();
-  final TextEditingController _idEditingController = TextEditingController();
-
-  bool isWindowMinimized = false;
-
-  final AllPeersLoader _allPeersLoader = AllPeersLoader();
-
-  // https://github.com/flutter/flutter/issues/157244
-  Iterable<Peer> _autocompleteOpts = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _allPeersLoader.init(setState);
-    _idFocusNode.addListener(onFocusChanged);
-    if (_idController.text.isEmpty) {
-      WidgetsBinding.instance.addPostFrameCallback((_) async {
-        final lastRemoteId = await bind.mainGetLastRemoteId();
-        if (lastRemoteId != _idController.id) {
-          setState(() {
-            _idController.id = lastRemoteId;
-          });
-        }
-      });
-    }
-    Get.put<TextEditingController>(_idEditingController);
-    Get.put<IDTextEditingController>(_idController);
-    windowManager.addListener(this);
-  }
-
-  @override
-  void dispose() {
-    _idController.dispose();
-    windowManager.removeListener(this);
-    _allPeersLoader.clear();
-    _idFocusNode.removeListener(onFocusChanged);
-    _idFocusNode.dispose();
-    _idEditingController.dispose();
-    if (Get.isRegistered<IDTextEditingController>()) {
-      Get.delete<IDTextEditingController>();
-    }
-    if (Get.isRegistered<TextEditingController>()) {
-      Get.delete<TextEditingController>();
-    }
-    super.dispose();
-  }
-
-  @override
-  void onWindowEvent(String eventName) {
-    super.onWindowEvent(eventName);
-    if (eventName == 'minimize') {
-      isWindowMinimized = true;
-    } else if (eventName == 'maximize' || eventName == 'restore') {
-      if (isWindowMinimized && isWindows) {
-        // windows can't update when minimized.
-        Get.forceAppUpdate();
-      }
-      isWindowMinimized = false;
-    }
-  }
-
-  @override
-  void onWindowEnterFullScreen() {
-    // Remove edge border by setting the value to zero.
-    stateGlobal.resizeEdgeSize.value = 0;
-  }
-
-  @override
-  void onWindowLeaveFullScreen() {
-    // Restore edge border to default edge size.
-    stateGlobal.resizeEdgeSize.value = stateGlobal.isMaximized.isTrue
-        ? kMaximizeEdgeSize
-        : windowResizeEdgeSize;
-  }
-
-  @override
-  void onWindowClose() {
-    super.onWindowClose();
-    bind.mainOnMainWindowClose();
-  }
-
-  void onFocusChanged() {
-    _idInputFocused.value = _idFocusNode.hasFocus;
-    if (_idFocusNode.hasFocus) {
-      if (_allPeersLoader.needLoad) {
-        _allPeersLoader.getAllPeers();
-      }
-
-      final textLength = _idEditingController.value.text.length;
-      // Select all to facilitate removing text, just following the behavior of address input of chrome.
-      _idEditingController.selection =
-          TextSelection(baseOffset: 0, extentOffset: textLength);
-    }
-  }
-
+class _ConnectionPageState extends State<ConnectionPage> {
   @override
   Widget build(BuildContext context) {
     final isOutgoingOnly = bind.isOutgoingOnly();
     return Column(
       children: [
         Expanded(
-            child: Column(
-          children: [
-            Row(
-              children: [
-                Flexible(child: _buildRemoteIDTextField(context)),
-              ],
-            ).marginOnly(top: 22),
-            SizedBox(height: 12),
-            Divider().paddingOnly(right: 12),
-            Expanded(child: PeerTabPage()),
-          ],
-        ).paddingOnly(left: 12.0)),
+          child: Container(), // Празен контейнер, за да премахнем дясната част
+        ),
         if (!isOutgoingOnly) const Divider(height: 1),
-        if (!isOutgoingOnly) OnlineStatusWidget()
+        if (!isOutgoingOnly) OnlineStatusWidget(), // Запазваме статусния индикатор
       ],
     );
-  }
-
-  /// Callback for the connect button.
-  /// Connects to the selected peer.
-  void onConnect({bool isFileTransfer = false}) {
-    var id = _idController.id;
-    connect(context, id, isFileTransfer: isFileTransfer);
-  }
-
-  /// UI for the remote ID TextField.
-  /// Search for a peer.
-
   }
 }
